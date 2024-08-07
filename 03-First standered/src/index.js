aae3989a8161ff2808434fc22cab3cef7ce3093e
nirbhayVaghela92 <nirbhayVaghela92@gmail.com>
2024-08-07 20:17:01 +0530

    connect  to mongo atlas

---
+++ 03-First standered/src/index.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+connectDB();
+
+/*
+import express from "express";
+import {DB_NAME} from "./constant.js"
+
+const app = express();
+
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODN_URI}/${DB_NAME}`);
+        app.on("error", () => {
+            console.log('Error: ', error);
+            throw error;
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listen on port:  ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.log('ERROR: ', error);
+        throw error;
+    }
+})()
+*/   
